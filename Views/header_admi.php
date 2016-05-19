ccfc8e0b685173a6ba1470c11f88e8d67b1aecaa
cristian9510 <cristian095@hotmail.es>
2016-05-19 15:37:11 -0500

    puse los estilos

--- Views/header_admi.php
+++ Views/header_admi.php
@@ -1,3 +1,16 @@
 <header>
-	<div>aaaa</div>
+	<div id="dvLogo">aaaa</div>
+	<div id="logn">Cerrar session</div>
 </header>
+<ul>
+	<li>Usuario</li>
+	<li>Juegos</li>
+	<li>Consolas</li>
+	<li>Categorias</li>
+	<li>Puntos</li>
+	<li>Bono</li>
+	<li>Encuestas</li>
+	<li>Sancion</li>
+	<li>Puntos-x-juego</li>
+	<li>Informes</li>
+</ul>
---
+++ Views/index_admi.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administrador</title>
+	<link rel="stylesheet" type="text/css" href="stylesSheet/gstyle_admi.css"/>
+	<link rel="stylesheet" type="text/css" href="stylesSheet/estilo.css"/>
+</head>
+<body>
+<?php include("header_admi.php");?>
+</body>
+</html>
