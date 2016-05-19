e7ff6cb97e926f096a0661e3e30f77b262e9f2b2
1014793 <acorrea829@misena.edu.co>
2016-05-19 17:05:07 -0500

    CREE EL BOTON PARA GUARDAR

--- Views/admin_consola.php
+++ Views/admin_consola.php
@@ -1,14 +1,17 @@
-
+<link rel="stylesheet" type="text/css" href="stylesSheet/estilo.css"/>
 <div id="cuadro">
 <h1 style="color: white">CONSOLA</h1>
 </div>
 <section>
 
 	<article id="admin_consola">
-		
-		<label>codigo  </label><input type="" /><br><br>
+	
+		<form action="usuarios.controller.php" method="post">
 
-		<label>nombre  </label><input type="" /><br><br>
-		<button>enviar</button>
+		<label for="txtCodigo">codigo  </label><input type="" /><br><br>
+
+		<label for="txtName">nombre  </label><input type="" /><br><br>
+
+		<button name="action" value="guardarconsola">enviar</button>
 
 	</article>
