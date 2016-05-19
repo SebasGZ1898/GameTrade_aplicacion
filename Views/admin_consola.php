fad7e58aa08160d8391c3277aca6989074646ebc
1014793 <acorrea829@misena.edu.co>
2016-05-19 16:43:35 -0500

    Mis cambios

---
+++ Views/admin_consola.php
@@ -0,0 +1,14 @@
+
+<div id="cuadro">
+<h1 style="color: white">CONSOLA</h1>
+</div>
+<section>
+
+	<article id="admin_consola">
+		
+		<label>codigo  </label><input type="" /><br><br>
+
+		<label>nombre  </label><input type="" /><br><br>
+		<button>enviar</button>
+
+	</article>
---
+++ Views/admin_usuario.php
@@ -0,0 +1,29 @@
+<h1>USUARIO</h1>
+
+
+	<article id="art_left_admin">
+		<h2>BUCAR USUARIOS</h2>
+		<input type="" />
+		<button>buscar</button>
+
+	</article>
+
+	<article id="art_center_admin">
+	<form>
+		<label>codigo   </label><input type="text" /><br><br>
+		<label>nombre   </label><input type="" /><br><br>
+		<label>apellido </label><input type="" /><br><br>
+		<label>telefono </label><input type="" /><br><br>
+		<label>correo     <input type="" /><br><br>
+		<label>celular  <input type="" /><br><br>
+		<label>ciudad   <input type="" /><br><br>
+		<label>puntos   <input type="" /><br><br>
+		
+	</form>
+	</article>
+
+
+
+
+</body>
+</html>
