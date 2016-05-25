84aaf3a8f9080553df4f7a7a74e92448c58d005d
cristian9510 <cristian095@hotmail.es>
2016-05-25 13:48:39 -0500

    creacion de nombres

--- Views/index_admi.php
+++ Views/index_admi.php
@@ -4,6 +4,7 @@
 	<title>Administrador</title>
 	<link rel="stylesheet" type="text/css" href="stylesSheet/gstyle_admi.css"/>
 	<link rel="stylesheet" type="text/css" href="stylesSheet/estilo.css"/>
+	<script type="text/javascript" src="js/gmindex_admi.js"></script>
 </head>
 <body>
 <?php include("header_admi.php");?>
@@ -14,35 +15,35 @@
 	<div id="dvAdm">Sebastian</div>
 </section>
 <section id="stA_User">
-
+usuario
 </section>
 
 <section id="stA_Game">
-
+juego
 </section>
 <section id="stA_Consola">
-
+consoola
 </section>
 <section id="stA_Categoria">
-
+categorias
 </section>
 <section id="stA_Puntos">
-
+puntos
 </section>
 <section id="stA_Bono">
-
+bono
 </section>
 <section id="stA_Encuesta">
-
+encuesta
 </section>
 <section id="stA_Sancion">
-
+sancion
 </section>
 <section id="stA_Puntosjue">
-
+puntosjue
 </section>
 <section id="stA_Inform">
-
+<h2>Todavia no se han creado los informes</h2>
 </section>
 
 </body>
