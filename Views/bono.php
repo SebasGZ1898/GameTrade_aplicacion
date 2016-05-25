b1ce0cb6920af00608095975494d6b13ca1619fb
wiltonan <wilton.anre13@hotmail.com>
2016-05-25 13:11:11 -0500

    cree bono

---
+++ Views/bono.php
@@ -0,0 +1,19 @@
+<link type="text/css" rel="stylesheet" href="esttilo.css">
+<meta charset="utf-8">
+
+<div class="formulario">
+  <form action="accion.php" method="post">
+  <h1>bono</h1>
+  <label>Código: </label>
+  <input type="text" name="bono_cod"/>
+  <label>Codigo de punto: </label>
+  <input type="text" name="pto_cod"/>
+  <label>Descripción de bono:</label>
+  <input type="text" name="bono_desc"/>
+  <label>Cantidad de bonos:</label> 
+  <input type="text" name="bono_cant"/>
+
+  <button type="button">Guardar</button>
+
+  </form>
+</div>
