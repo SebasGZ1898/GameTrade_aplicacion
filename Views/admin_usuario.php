e5f86b6109bf274ee7d03b12b6a81930921bbab6
1014793 <acorrea829@misena.edu.co>
2016-05-25 17:08:54 -0500

    creasion de usuario admin

--- Views/admin_usuario.php
+++ Views/admin_usuario.php
@@ -1,5 +1,7 @@
-<h1>USUARIO</h1>
-
+<div id="cuadro">
+<h1 style="color: white">USUARIO</h1>
+</div>
+<section>
 
 	<article id="art_left_admin">
 		<h2>BUCAR USUARIOS</h2>
@@ -10,14 +12,29 @@
 
 	<article id="art_center_admin">
 	<form>
-		<label>codigo   </label><input type="text" /><br><br>
-		<label>nombre   </label><input type="" /><br><br>
-		<label>apellido </label><input type="" /><br><br>
-		<label>telefono </label><input type="" /><br><br>
-		<label>correo     <input type="" /><br><br>
-		<label>celular  <input type="" /><br><br>
-		<label>ciudad   <input type="" /><br><br>
-		<label>puntos   <input type="" /><br><br>
+
+
+	<form action="../Controller/usuario.controller.php" method="post">
+
+		<label for="txtCodigo">codigo  </label><input type="text" name="txtCodigo"/> <br><br>
+
+		<label for="txtNombre">nombre  </label><input type="text" name="txtNombre" /><br><br>
+
+		<label for="txtApellido">apellido  </label><input type="text" name="txtApellido" /><br><br>
+
+		<label for="txtTelefono">Telefono  </label><input type="text" name="txtTelefono"/> <br><br>
+
+		<label for="txtCorreo">correo  </label><input type="text" name="txtCorreo" /><br><br>
+
+		<label for="txtCelular">celular  </label><input type="text" name="txtCelular" /><br><br>
+
+		<label for="txtCiudad">ciudad  </label><input type="text" name="txtCiudad" /><br><br>
+
+		<label for="txtPuntos">puntos </label><input type="text" name="txtPuntos" /><br><br>
+
+
+		<button name="action" value="guardarusuario">enviar</button>
+
 		
 	</form>
 	</article>
