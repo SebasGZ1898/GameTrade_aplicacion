d3240177493c7193d8b268f120fa1d005c72138e
wiltonan <wilton.anre13@hotmail.com>
2016-05-25 17:21:28 -0500

    bono

--- Views/bono.php
+++ Views/bono.php
@@ -1,19 +1,33 @@
 <!--aqui esta el formato y de esta de esta forma se hace -->
-<div class="formulario">
-  <form action="../Controller/bono.controller.php" method="post">
-  <h1>Bono</h1>
-  <label>Código: </label>
-  <input type="text" name="bono_cod"/>
-  <label>Código de categoria de bono: </label>
-  <input type="text" name="cat_bono_cod"/>
-  <label>Codigo de punto: </label>
-  <input type="text" name="pto_cod"/>
-  <label>Descripción de bono:</label>
-  <input type="text" name="bono_desc"/>
-  <label>Cantidad de bonos:</label>
-  <input type="text" name="bono_cant"/>
-  <!--este es el boton-->
-  <button name="action" value="guardarbono">Guardar</button>
-  </form>
 
+<div class="titulo_bono">
+  <h1>Bono</h1>
+</div>
+<div class="formulario">
+
+  <form action="../Controller/bono.controller.php" method="post">
+
+    <label>Código: </label>
+    <input type="text" name="bono_cod"/>
+    </br>
+    </br>
+    <label>Código de categoria de bono: </label>
+    <input type="text" name="cat_bono_cod"/>
+    </br>
+    </br>
+    <label>Codigo de punto: </label>
+    <input type="text" name="pto_cod"/>
+    </br>
+    </br>
+    <label>Descripción de bono:</label>
+    <input type="text" name="bono_desc"/>
+    </br>
+    </br>
+    <label>Cantidad de bonos:</label>
+    <input type="text" name="bono_cant"/>
+    </br>
+    </br>
+    <!--este es el boton-->
+    <button name="action" value="guardarbono">Guardar</button>
+  </form>
 </div>
