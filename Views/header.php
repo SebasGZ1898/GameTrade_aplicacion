7893854878648a5b08e09b2481d997e466658931
cristian9510 <cristian095@hotmail.es>
2016-07-14 10:06:43 -0500

    cambios

--- Views/header.php
+++ Views/header.php
@@ -1,24 +1,34 @@
 <header>
 	<div id="dvLogo"></div>
 	<div id="dvContel">
-		<form action="../Controller/ValidarUsu.Controller.php" method="post">
-		<ul id="login">
-			<li><input type="text" placeholder="Correo Electronico" name="txtCode" id="txtCode"/></li>
-			<li><input type="password" placeholder="*************" name="txtName" id="txtName"/></li>
-			<li><button  name="btnusu" id="btnOpen" value="ingresoUsu">Enviar</button></li>
-		</ul>
-		</form>
+		<div id="invitLogin">Ingresa ya a nuestra pagina</div>
+		<div id="dvLogin">
+			<form action="../Controller/ValidarUsu.Controller.php" method="post">
+			<ul id="Login">
+				<li id="dvCode"><input type="text" placeholder="Correo Electronico" name="txtCode" id="txtCode"/></li>
+				<li><input type="password" placeholder="*************" name="txtName" id="txtName"/></li>
+			</ul>
+			<div id="dvButonL"><button  name="btnusu" id="btnOpen" value="ingresoUsu"></button></div>
+			</form>
+			</div>
+
 		<ul id="ulRecord">
-			<li>Recordar Contraseña</li>
-			<li>Registrarse</li>
+			<li>Olvide mi Contraseña</li>
+			<li>Registrate</li>
 		</ul>
 	</div>
 </header>
-<nav>
+<ul id="dvMenu">
+	<li id="code"></li>
+	<li id="liwor"></li>
+	<li id="licontact"></li>
+	<li id="liUsa"></li>
+</ul>
+<!--<nav>
 	<ul id="ulMenu">
 		<li>Inicio</li>
 		<li>Quienes Somos</li>
 		<li>Contactenos</li>
 		<li>Como se usa</li>
 	</ul>
-</nav>
+</nav>-->
--- Views/index.php
+++ Views/index.php
@@ -2,9 +2,14 @@
 <head>
 	<title>GAMETRADE</title>
 	<link rel="stylesheet" type="text/css" href="stylesSheet/gstyle.css"/>
-	<script type="text/javascript" src=""></script>
+	<script type="text/javascript" src="hh"></script>
 </head>
 <body>
-<?php require_once("header.php"); ?>
+
+<div id="dv_main">
+	<?php require_once("header.php"); ?>
+	<div id="dvSlider"></div>
+</div>
+
 </body>
 </html>
