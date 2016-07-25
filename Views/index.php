0eb8117cf04962e417fb1be18f966965ef2ede29
cristian9510 <cristian095@hotmail.es>
2016-07-25 10:17:41 -0500

    creacion de index

--- Views/index.php
+++ Views/index.php
@@ -2,6 +2,7 @@
 <head>
 	<title>GAMETRADE</title>
 	<link rel="stylesheet" type="text/css" href="stylesSheet/gstyle.css"/>
+	<link rel="stylesheet" type="text/css" href="stylesSheet/mindex.css"/>
 	<script type="text/javascript" src="hh"></script>
 </head>
 <body>
@@ -9,6 +10,37 @@
 <div id="dv_main">
 	<?php require_once("header.php"); ?>
 	<div id="dvSlider"></div>
+	<div id="e64Video">
+	</div>
+
+	<div id="e64ContePublic">
+		<div id="e64Public1"></div>
+		<div id="e64Public2"></div>
+	</div>
+<div id="e64Destacado">Destacados</div>
+<div id="e64SliderVideo">Slider videos</div>
+<div id="e63JueDestac">VideosJuegos mas descatados</div>
+<ul id="ulNoticJue">
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
+<ul id="ulNoticJue">
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
+
+<div id="e64Noticia">noticias</div>
+
+<div id="e64ConteNoti">
+	<div id="e64Noticia1"></div>
+	<div id="e64SlideNoticia">Slider</div>
+</div>
+
+<div id="e64Public3"></div>
 </div>
 
 </body>
