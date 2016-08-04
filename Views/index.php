f69cc40512259ba5bc779d809359089981e5615f
1014793 <acorrea829@misena.edu.co>
2016-08-04 16:58:20 -0500

    contactenos

--- Views/index.php
+++ Views/index.php
@@ -7,17 +7,16 @@
 	<link rel="stylesheet" type="text/css" href="stylesSheet/Slider.css"/>
 	<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
 	<script type="text/javascript" src="js/mindexC.js"></script>
-	<script type="text/javascript" src="js/gweb.js"></script>
 </head>
 <body>
 
 <div id="dv_main">
 	<?php require_once("header.php"); ?>
 
-	<!--<div id="e64Monstrar">
+	<div id="e64Monstrar">
 		<div id="e64ClosPrinci">X</div>
 		<div id="e64ContePrinci"></div>
-	</div>-->
+	</div>
 
 
 
@@ -56,34 +55,6 @@
 		<div id="e64Public2"></div>
 	</div>
 
-
-<div id="e63JueDestac">VideosJuegos mas descatados</div>
-<ul id="ulNoticJue">
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-</ul>
-<ul id="ulNoticJue">
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-	<li></li>
-</ul>
-
-<div id="e64Noticia">noticias</div>
-
-<div id="e64ConteNoti">
-	<div id="e64Noticia1"></div>
-	<div id="e64SlideNoticia">
-		
-</body>
-	</div>
-</div>
-
-<div id="e64Public3"></div>
 </div>
 
 </body>
