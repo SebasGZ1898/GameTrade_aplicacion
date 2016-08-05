32d853cbb1d3d5bce343eebdd3aef5dc8a770d2e
cristian9510 <cristian095@hotmail.es>
2016-08-05 07:34:35 -0500

    Nuevo archivo para el registrar

---
+++ Views/registro.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Registro</title>
+		<meta charset="utf8"/>
+		<link rel="stylesheet" type="text/css" href="stylesSheet/gstyle.css"/>
+
+		<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+		<script type="text/javascript" src="js/gweb.js"></script>
+</head>
+<body>
+
+	<div id="dv_main">
+	<?php require_once("header.php"); ?>
+</div>
+</body>
+</html>
